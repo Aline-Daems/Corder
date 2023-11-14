11136b9d447c4547abcff869662887cc5214d553
Aline Daems <daems.aline90@gmail.com>
2023-11-14 10:21:29 +0100

    first commit

---
+++ src/main/java/be/technobel/corder/CorderApplication.java
@@ -0,0 +1,13 @@
+package be.technobel.corder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorderApplication.class, args);
+    }
+
+}
