c55582182ea98cda51ffd2fedf1720d035801704
lucas <lucasbalon@gmail.com>
2023-11-14 15:45:18 +0100

    Implement User service with repository
    Extended UserServiceImpl to implement UserService. UserServiceImpl now interacts with UserRepository to create, update, find, and delete user entities in the database. The UserRepository is autowired for dependency injection. The changes allow more efficient data manipulation for User data.

--- src/main/java/be/technobel/corder/bl/impl/UserServiceImpl.java
+++ src/main/java/be/technobel/corder/bl/impl/UserServiceImpl.java
@@ -1,4 +1,53 @@
 package be.technobel.corder.bl.impl;
 
-public class UserServiceImpl {
+import be.technobel.corder.bl.UserService;
+import be.technobel.corder.dal.models.User;
+import be.technobel.corder.dal.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(Long id, User user) {
+        User user1 = findById(id);
+        user1.setFirstName(user.getFirstName());
+        user1.setLastName(user.getLastName());
+        user1.setLogin(user.getLogin());
+        user1.setPassword(user.getPassword());
+        user1.setRole(user.getRole());
+        user1.setEmail(user.getEmail());
+        return userRepository.save(user1);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User delete(Long id) {
+        User user = findById(id);
+        userRepository.delete(user);
+        return user;
+    }
 }
