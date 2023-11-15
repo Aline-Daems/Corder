6af061c61ddbca148d914abe63c6fc8139672a7d
lucas <lucasbalon@gmail.com>
2023-11-15 14:47:01 +0100

    Add CRUD, validation, and shipping endpoints to ParticipationController
    Expanded ParticipationController functionality by implementing new endpoints for retrieving lists of all participation entries, validated entries, non-validated entries, shipped entries and non-shipped entries. Also added new endpoints for deleting, validating, and shipping single entries by id.

--- src/main/java/be/technobel/corder/pl/controllers/ParticipationController.java
+++ src/main/java/be/technobel/corder/pl/controllers/ParticipationController.java
@@ -5,9 +5,15 @@ import be.technobel.corder.dal.models.Participation;
 
 import be.technobel.corder.pl.models.dtos.ParticipationDTO;
 import be.technobel.corder.pl.models.forms.ParticipationForm;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/participation")
@@ -38,5 +44,60 @@ public class ParticipationController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<ParticipationDTO>> findAll() {
+        List<Participation> participations = participationService.findAll();
+        List<ParticipationDTO> dtos = participations.stream()
+                .map(ParticipationDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    @GetMapping("/allValidated")
+    public ResponseEntity<List<ParticipationDTO>> findValidated() {
+        List<Participation> participations = participationService.findValidated();
+        List<ParticipationDTO> dtos = participations.stream()
+                .map(ParticipationDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    @GetMapping("/allNonValidated")
+    public ResponseEntity<List<ParticipationDTO>> findNonValidated() {
+        List<Participation> participations = participationService.findNonValidated();
+        List<ParticipationDTO> dtos = participations.stream()
+                .map(ParticipationDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    @GetMapping("/allShipped")
+    public ResponseEntity<List<ParticipationDTO>> findShipped() {
+        List<Participation> participations = participationService.findShipped();
+        List<ParticipationDTO> dtos = participations.stream()
+                .map(ParticipationDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    @GetMapping("/allNonShipped")
+    public ResponseEntity<List<ParticipationDTO>> findNonShipped() {
+        List<Participation> participations = participationService.findNonShipped();
+        List<ParticipationDTO> dtos = participations.stream()
+                .map(ParticipationDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(dtos);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        participationService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PostMapping("/validate/{id}")
+    public ResponseEntity<Boolean> validate(@PathVariable Long id) {
+        boolean result = participationService.validate(id);
+        return ResponseEntity.ok(result);
+    }
+    @PostMapping("/ship/{id}")
+    public ResponseEntity<Boolean> ship(@PathVariable Long id) {
+        boolean result = participationService.ship(id);
+        return ResponseEntity.ok(result);
     }
 }
