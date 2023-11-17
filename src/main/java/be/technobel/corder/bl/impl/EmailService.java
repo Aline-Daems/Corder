cbeff49a722f6942c85404658e5d255020cf7063
lucas <lucasbalon@gmail.com>
2023-11-17 16:32:51 +0100

    Add email service and update config files (Pas encore testé)
    This commit introduces an EmailService and modifies the ParticipationService to use this new Email Service. In addition, several dependencies related to security and mailing have been added to the pom.xml file. Lastly, a new MailConfiguration class and environment variable changes to the application.yml file have been made to support the email functionality.

---
+++ src/main/java/be/technobel/corder/bl/impl/EmailService.java
@@ -0,0 +1,22 @@
+package be.technobel.corder.bl.impl;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
---
+++ src/main/java/be/technobel/corder/pl/config/mail/MailConfiguration.java
@@ -0,0 +1,31 @@
+package be.technobel.corder.pl.config.mail;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+    private final Environment env;
+
+    public MailConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public JavaMailSender mailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("spring.mail.port")));
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", env.getProperty("spring.mail.properties.mail.smtp.auth"));
+        props.put("mail.smtp.starttls.enable", env.getProperty("spring.mail.properties.mail.smtp.starttls.enable"));
+        return mailSender;
+    }
+}
