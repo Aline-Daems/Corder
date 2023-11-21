3fd4fa042b2ef6a48e45b74eb35b85a4d7e7fa50
lucas <lucasbalon@gmail.com>
2023-11-21 14:49:00 +0100

    Add rate limit interceptor and Bucket4j dependency
    Added a new interceptor, RateLimitInterceptor, for rate limiting requests based on IP. Also, included Bucket4j library into the project dependencies, which is utilized by the interceptor to handle IP based request rates. In the event of too many requests, the server will respond with HTTP 429 status.

---
+++ src/main/java/be/technobel/corder/pl/config/interceptor/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package be.technobel.corder.pl.config.interceptor;
+
+import be.technobel.corder.pl.interceptor.RateLimitInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RateLimitInterceptor());
+    }
+}
---
+++ src/main/java/be/technobel/corder/pl/interceptor/RateLimitInterceptor.java
@@ -0,0 +1,35 @@
+package be.technobel.corder.pl.interceptor;
+
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RateLimitInterceptor implements HandlerInterceptor {
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket createNewBucket() {
+        return Bucket.builder()
+                .addLimit(limit -> limit
+                        .capacity(50)
+                        .refillGreedy(50, Duration.ofMinutes(1)))
+                .build();
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String ip = request.getRemoteAddr();
+        System.out.println(ip);
+        Bucket bucket = buckets.computeIfAbsent(ip, k -> createNewBucket());
+
+        if (bucket.tryConsume(1)) {
+            return true;
+        } else {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value()); // 429
+            return false;
+        }
+    }
+}
