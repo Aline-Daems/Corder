accd083892541a5d02d24f31b5758e223318dcce
lucas <lucasbalon@gmail.com>
2023-12-06 14:26:36 +0100

    Added Address implementation and interfaces

---
+++ src/main/java/be/technobel/corder/bl/AddressService.java
@@ -0,0 +1,14 @@
+package be.technobel.corder.bl;
+
+import be.technobel.corder.dal.models.Address;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AddressService {
+    void create(Address address);
+    void update(Long id, Address address);
+    Optional<Address> findById(Long id);
+    List<Address> findAll();
+    void delete(Long id);
+}
---
+++ src/main/java/be/technobel/corder/bl/impl/AddressServiceImpl.java
@@ -0,0 +1,54 @@
+package be.technobel.corder.bl.impl;
+
+import be.technobel.corder.bl.AddressService;
+import be.technobel.corder.dal.models.Address;
+import be.technobel.corder.dal.repositories.AddressRepository;
+import jakarta.ws.rs.NotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public void create(Address address) {
+        Address entity = new Address();
+        entity.setCity(address.getCity());
+        entity.setStreet(address.getStreet());
+        entity.setPostCode(address.getPostCode());
+        addressRepository.save(entity);
+    }
+
+
+    @Override
+    public void update(Long id, Address address) {
+        Address entity = addressRepository.findById(id).orElseThrow(()-> new NotFoundException("Participation non trouvée"));
+        entity.setCity(address.getCity());
+        entity.setStreet(address.getStreet());
+        entity.setPostCode(address.getPostCode());
+       addressRepository.save(entity);
+    }
+
+    @Override
+    public Optional<Address> findById(Long id) {
+        return addressRepository.findById(id);
+    }
+
+    @Override
+    public List<Address> findAll() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public void delete(Long id) {
+        addressRepository.deleteById(id);
+    }
+}
