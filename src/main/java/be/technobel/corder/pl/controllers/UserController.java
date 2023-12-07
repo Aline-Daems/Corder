721b587633ae957eb3aa2e0b11fcec388c6f7709
lucas <lucasbalon@gmail.com>
2023-12-07 13:58:41 +0100

    Update UserController response after user creation
    Modified the "create" method in UserController to directly return a response entity with a success message after a user is created, instead of returning the user outcome from the UserService. This change enhances the readability and meaningfulness of the response message.

--- src/main/java/be/technobel/corder/pl/controllers/UserController.java
+++ src/main/java/be/technobel/corder/pl/controllers/UserController.java
@@ -26,7 +26,8 @@ public class UserController {
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody UserForm form) {
         try {
-            return ResponseEntity.ok(userService.create(form));
+            userService.create(form);
+            return ResponseEntity.ok("User created");
         } catch (DuplicateParticipationException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
